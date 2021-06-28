2a4dc896b166f71bff9849d6200f9f3c689961a6
Oleh <Bess.Oleg73@gmail.com>
2021-06-28 12:58:30 +0300

    GeoConverter

---
+++ GeoConverter/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GeoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьФайлыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьФайлыToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выбратьФайлыToolStripMenuItem
+            // 
+            this.выбратьФайлыToolStripMenuItem.Name = "выбратьФайлыToolStripMenuItem";
+            this.выбратьФайлыToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.выбратьФайлыToolStripMenuItem.Text = "Выбрать файлы";
+            this.выбратьФайлыToolStripMenuItem.Click += new System.EventHandler(this.выбратьФайлыToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 178);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 250);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GeoCoverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьФайлыToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ GeoConverter/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace GeoConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        bool flag1=false,flag2=false;
+        string[] files;
+        string file,path=@"ConvertFiles\",format= ".geo";
+       // string covertpath = @"Cenverts";
+
+        private void выбратьФайлыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = null;
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                files = openFileDialog1.FileNames;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    richTextBox1.Text += files[i] + "\n" + "\n";
+                    openFileDialog1.InitialDirectory = Path.GetDirectoryName(files[i]);
+                }
+            }
+            
+            flag2 = true;
+            flag1 = false;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+          //  richTextBox1.Text = null;
+            if (flag1 && file != null)
+            {
+                openFileDialog1.InitialDirectory = Path.GetDirectoryName(files[files.Length-1]);
+                Thread tre = new Thread(covertfile);
+                tre.Start();
+            }
+            if (flag2&& files != null)
+            {
+                // Thread tres = new Thread(covertfiles);
+                //tres.Start();
+                covertfiles();
+            }
+            if(flag1==flag2) {
+                richTextBox1.Text = "для работы програмы выберите файл или файлы";
+            }
+            //covertfile();
+        }
+        private void covertfile()
+        {
+            if (file.EndsWith(".json"))
+            {
+                List<string> strfile;
+                string _lines, r; _lines = System.IO.File.ReadAllText(file, Encoding.GetEncoding(1251));
+                int index1, index2;
+                index1 = _lines.IndexOf("[[");
+                r = _lines.Substring(index1);
+                index2 = r.IndexOf("]]],");
+                index2 = index2 + 1;
+                r = r.Substring(0, index2);
+                r = r.Replace("]]", " ;");
+                r = r.Replace("]", "");
+                r = r.Replace(",", " ");
+                string[] g = r.Split(new char[] { '[', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] g1;
+                string str = null;
+                strfile = new List<string> { };
+                StreamWriter f = new StreamWriter(path + "convert" + format);
+                for (int i = 0, j = 0, num = 0, kol = 0; i < g.Length; i++)
+                {
+                    if (g[i].IndexOf(";") == -1)
+                    {
+                        if (j == 0 || j % 2 == 0)
+                        {
+                            str = g[i].Remove(0, 1);
+                            j++;
+                        }
+                        else
+                        {
+                            num++;
+                            strfile.Add(num + " " + g[i] + " " + str + " 100");
+                            j++;
+                            kol++;
+                        }
+                    }
+                    else
+                    {
+                        g1 = strfile.ToArray<string>();
+                        f.WriteLine(kol);
+                        for (int b = 0; b < g1.Length; b++)
+                        {
+                            f.WriteLine(g1[b]);
+                        }
+                        strfile = new List<string> { };
+                        kol = 0;
+                    }
+
+                }
+                f.Close();
+            }
+            
+        }
+
+        private void covertfiles()
+        {
+            List<string> strfile;string r, _lines;
+           
+            for (int fi = 0; fi < files.Length; fi++)
+            {
+                if (files[fi].EndsWith(".json"))
+                {
+                    _lines = System.IO.File.ReadAllText(files[fi], Encoding.GetEncoding(1251));
+                    int index1, index2;
+                    index1 = _lines.IndexOf("[[");
+                    r = _lines.Substring(index1);
+                    index2 = r.IndexOf("]]],");
+                    index2 = index2 + 1;
+                    r = r.Substring(0, index2);
+                    r = r.Replace("]]", " ;");
+                    r = r.Replace("]", "");
+                    r = r.Replace(",", " ");
+                    string[] g = r.Split(new char[] { '[', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    string[] g1;
+                    string str = null;
+                    strfile = new List<string> { };
+                    string u = files[fi].Remove(0,files[fi].LastIndexOf("\\")+1);
+                    u = u.Replace(".json", "");
+                    StreamWriter f = new StreamWriter(path + u + format);
+                    for (int i = 0, j = 0, num = 0, kol = 0; i < g.Length; i++)
+                    {
+                        if (g[i].IndexOf(";") == -1)
+                        {
+                            if (j == 0 || j % 2 == 0)
+                            {
+                                str = g[i].Remove(0, 1);
+                                j++;
+                            }
+                            else
+                            {
+                                num++;
+                                strfile.Add(num + " " + g[i] + " " + str + " 100");
+                                j++;
+                                kol++;
+                            }
+                        }
+                        else
+                        {
+                            g1 = strfile.ToArray<string>();
+                            f.WriteLine(kol);
+                            for (int b = 0; b < g1.Length; b++)
+                            {
+                                f.WriteLine(g1[b]);
+                            }
+                            
+                            strfile = new List<string> { };
+                            kol = 0;
+                        }
+
+                    } f.Close();
+                }
+                
+               
+            }
+            MessageBox.Show("Конвертирование завершено","Уведомление",MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
